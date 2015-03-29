861b165705b48263557eb8704ed6872d342a4c5f
kannans <kannanhudson@gmail.com>
2015-03-29 19:23:44 +0530

    Login functionality added for todo... Rocking...

--- meteorjs-todos.js
+++ meteorjs-todos.js
@@ -3,6 +3,10 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
   Template.body.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")) {
@@ -27,10 +31,7 @@ if (Meteor.isClient) {
 
     var text = event.target.text.value;
 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
+    Meteor.call("addTask", text);
 
     // Clear form
     event.target.text.value = "";
@@ -40,14 +41,35 @@ if (Meteor.isClient) {
   },
   "click .toggle-checked": function () {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    Meteor.call("setChecked", this._id, ! this.checked);
   },
   "click .delete": function () {
-    Tasks.remove(this._id);
+    Meteor.call("deleteTask", this._id);
   },
   "change .hide-completed input": function (event) {
     Session.set("hideCompleted", event.target.checked);
   }
 });
-  
+
 }
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
